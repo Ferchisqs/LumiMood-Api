8cce1066f480f4520853bb29fa63558dcaaf3686
Ferchisqs <selihgamer6@gmail.com>
2025-03-16 17:16:31 -0600

    peticiones

--- src/index.js
+++ src/index.js
@@ -2,11 +2,14 @@ const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const dotenv = require('dotenv');
+const morgan = require('morgan');
 
 // require('../src/utils/scheduler');
 dotenv.config();
 
 const app = express();
+
+app.use(morgan('dev'));  
 
 app.use(cors());
 app.use(bodyParser.json());
@@ -16,8 +19,7 @@ app.use('/api/emotions', require('./routes/emotions.routes'));
 app.use('/api/messages', require('./routes/messages.routes'));
 app.use('/api/recommendations', require('./routes/recommendations.routes'));
 
-
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`Servidor corriendo en http://localhost:${PORT}`);
+  console.log(`Servidor corriendo en ${PORT}`);
 });
